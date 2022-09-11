1f8f393b75617bc72072a850d0e627b0b6a1b815
T0wby <tobiasnuehlen@gmx.de>
2022-09-11 18:23:00 +0200

    Setting up Project

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player_Towby
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        
+        private PlayerControls _playerControls;
+        private InputAction _moveAround;
+        private InputAction _lookAround;
+
+
+        private Rigidbody _thisRb;
+        private Vector2 _look;
+        private Vector2 _move;
+        private Vector3 _direction;
+
+        [Header("Settings")]
+        [SerializeField] private float _walkSpeed = 10f;
+        [SerializeField] private float _runSpeed = 30f;
+
+        private void Awake()
+        {
+            _playerControls = new PlayerControls();
+            _thisRb = GetComponent<Rigidbody>();
+        }
+
+        private void OnEnable()
+        {
+            _moveAround = _playerControls.PlayerMovement.Movement;
+            _moveAround.Enable();
+
+            _lookAround = _playerControls.PlayerMovement.Camera;
+            _lookAround.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _moveAround.Disable();
+            _lookAround.Disable();
+        }
+
+        private void Update()
+        {
+            Debug.Log(_move);
+            Move(_move);
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            _move = context.ReadValue<Vector2>();
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            _look = context.ReadValue<Vector2>();
+        }
+
+        private void Move(Vector2 dir)
+        {
+            _direction = new Vector3(dir.x, 0, dir.y);
+
+            _thisRb.velocity += _direction * _walkSpeed * Time.deltaTime;
+        }
+    }
+}
+
