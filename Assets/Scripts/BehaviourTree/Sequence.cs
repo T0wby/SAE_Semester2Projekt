36675e5f0ef20c6329accbe05de4abfb06e9aa1e
T0wby <tobiasnuehlen@gmx.de>
2022-09-21 19:36:58 +0200

    -First draft of the behaviorTree

---
+++ Assets/Scripts/BehaviourTree/Sequence.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorTree
+{
+    public class Sequence : Node
+    {
+        public override ENodeState CalculateState()
+        {
+            return base.CalculateState();
+        }
+    } 
+}
