32b9200d562bd1b38cdd618dc4f90462ac6025cb
T0wby <tobiasnuehlen@gmx.de>
2022-09-21 16:25:38 +0200

    -Added collision detection

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -15,6 +15,7 @@ namespace Player_Towby
         private Vector3 _cameraPosition;
         private Vector3 _targetPosition;
         private Vector3 _rotation;
+        private Vector3 _cameraFollowVelocity = Vector3.zero;
         private Quaternion _targetRotation;
 
         [Header("Settings")]
@@ -23,6 +24,10 @@ namespace Player_Towby
         [SerializeField] private float _pivotSpeed;
         [SerializeField] private float _minimumPivot = -35f;
         [SerializeField] private float _maximumPivot = 35f;
+        [SerializeField] private float _cameraSphereRadius = 0.2f;
+        [SerializeField] private float _cameraCollisionOffset = 0.2f;
+        [SerializeField] private float _minimumCollisionOffset = 0.2f;
+        private float _cameraTargetPosition;
         private float _defaultPosition;
         private float _lookAngle;
         private float _pivotAngle;
@@ -36,8 +41,11 @@ namespace Player_Towby
 
         public void FollowTarget(float delta)
         {
-            _targetPosition = Vector3.Lerp(_thisTransform.position, _targetTransform.position, delta/_followSpeed);
+            //_targetPosition = Vector3.Lerp(_thisTransform.position, _targetTransform.position, delta/_followSpeed);
+            _targetPosition = Vector3.SmoothDamp(_thisTransform.position, _targetTransform.position, ref _cameraFollowVelocity, delta / _followSpeed);
             _thisTransform.position = _targetPosition;
+
+            HandleCameraCollision(delta);
         }
 
         public void HandleCameraRotation(float delta, float mouseX, float mouseY)
@@ -55,6 +63,28 @@ namespace Player_Towby
             _rotation.x = _pivotAngle;
             _targetRotation = Quaternion.Euler(_rotation);
             _cameraPivotTransform.localRotation = _targetRotation;
+        }
+
+        private void HandleCameraCollision(float delta)
+        {
+            _cameraTargetPosition = _defaultPosition;
+            RaycastHit hit;
+            Vector3 camPivDirection = _cameraTransform.position - _cameraPivotTransform.position;
+            camPivDirection.Normalize();
+
+            if (Physics.SphereCast(_cameraPivotTransform.position, _cameraSphereRadius, camPivDirection, out hit, Mathf.Abs(_cameraTargetPosition), _ignoreLayers))
+            {
+                float dis = Vector3.Distance(_cameraPivotTransform.position, hit.point);
+                _cameraTargetPosition = -(dis - _cameraCollisionOffset);
+            }
+
+            if (Mathf.Abs(_cameraTargetPosition) < _minimumCollisionOffset)
+            {
+                _cameraTargetPosition = -_cameraCollisionOffset;
+            }
+
+            _cameraPosition.z = Mathf.Lerp(_cameraTransform.localPosition.z, _cameraTargetPosition, delta / 0.2f);
+            _cameraTransform.localPosition = _cameraPosition;
         }
     }
 }
