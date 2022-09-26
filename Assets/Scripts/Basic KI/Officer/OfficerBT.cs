0320d3408c34feca50eadff488ee15dd0fdf3291
Towby <tobiasnuehlen@gmx.de>
2022-09-26 12:57:04 +0200

    -Added NavMesh -Added NavMeshAgent to Officer

--- Assets/Scripts/Basic KI/Officer/OfficerBT.cs
+++ Assets/Scripts/Basic KI/Officer/OfficerBT.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using BehaviorTree;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class OfficerBT : BehaviorTree.Tree
 {
     public Transform[] waypoints;
@@ -11,8 +13,13 @@ public class OfficerBT : BehaviorTree.Tree
     public static float fovRange = 6f;
     public static float attackRange = 2f;
 
+    private NavMeshAgent _agent;
+    public NavMeshAgent Agent { get { return _agent; } }
+
     protected override Node SetupTree()
     {
+        _agent = GetComponent<NavMeshAgent>();
+
         Node root = new Selector(new List<Node>
         {
             new Sequence(new List<Node>
