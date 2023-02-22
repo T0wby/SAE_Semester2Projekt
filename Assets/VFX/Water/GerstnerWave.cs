815fb68b663dcb234826e389bd044a34d6e15408
T0wby <tobiasnuehlen@gmx.de>
2023-02-22 11:24:32 +0100

    -Added 2nd wave

--- Assets/VFX/Water/GerstnerWave.cs
+++ Assets/VFX/Water/GerstnerWave.cs
@@ -7,11 +7,21 @@ public class GerstnerWave : MonoBehaviour
 {
     #region Fields
     [SerializeField] private ComputeShader _shader;
+
+    [Header("Wave One Settings")]
     [SerializeField] private float _waveAmplitude = 1f;
     [SerializeField] private float _waveWavelength = 10f;
     [SerializeField] private float _waveSteepness = 0.5f;
     [SerializeField] private Vector2 _waveDirection = Vector2.right;
     [SerializeField] private float _waveSpeed = 1f;
+
+    [Header("Wave Two Settings")]
+    [SerializeField] private float _waveAmplitude2 = 1f;
+    [SerializeField] private float _waveWavelength2 = 10f;
+    [SerializeField] private float _waveSteepness2 = 0.5f;
+    [SerializeField] private Vector2 _waveDirection2 = Vector2.right;
+    [SerializeField] private float _waveSpeed2 = 1f;
+
     private Mesh _mesh;
     private int _kernelHandle;
     private ComputeBuffer _vertexBuffer;
@@ -42,17 +52,26 @@ public class GerstnerWave : MonoBehaviour
         _vertexBuffer.SetData(_meshVertices);
 
         _shader.SetBuffer(_kernelHandle, "vertexBuffer", _vertexBuffer);
+
+        //Wave One
         _shader.SetFloat("waveAmplitude", _waveAmplitude);
         _shader.SetFloat("waveWavelength", _waveWavelength);
         _shader.SetFloat("waveSteepness", _waveSteepness);
         _shader.SetFloat("waveSpeed", _waveSpeed);
         _shader.SetVector("waveDirection", _waveDirection);
+        //Wave Two
+        _shader.SetFloat("waveAmplitude2", _waveAmplitude2);
+        _shader.SetFloat("waveWavelength2", _waveWavelength2);
+        _shader.SetFloat("waveSteepness2", _waveSteepness2);
+        _shader.SetFloat("waveSpeed2", _waveSpeed2);
+        _shader.SetVector("waveDirection2", _waveDirection2);
 
     }
 
     private void Update()
     {
         _shader.SetFloat("time", Time.time);
+        // For runtime changes add properties here
 
         _shader.Dispatch(_kernelHandle, _groupSizeX, 1, 1);
 
